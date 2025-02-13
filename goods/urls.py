c2aa9ebf2fc9eab74d7f364c46e030d750a6967f
MarkLev <levmarkpost@gmail.com>
2025-02-13 21:43:10 +0300

    наконецто сделал пагинацию

--- goods/urls.py
+++ goods/urls.py
@@ -1,17 +1,10 @@
-
 from django.urls import path
 
 from goods import views
 
-
 app_name = 'goods'
 
 urlpatterns = [
-    path('<slug:category_slug>', views.catalog, name='index'),
-<<<<<<< HEAD
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
-=======
->>>>>>> 1b16c7b (Normal pagination is added)
+    path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
-
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,46 +1,31 @@
-
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 from goods.models import Products
 
-<<<<<<< HEAD
-def catalog(request, category_slug, page=1):
-    if category_slug == 'all':
-        goods = Products.objects.all()
-    else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
-    paginator = Paginator(goods, 6)
-    current_page = paginator.page(page)
-=======
 def catalog(request, category_slug):
     page = request.GET.get('page', 1)
 
-    if category_slug == 'all':
+    if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
-    paginator = Paginator(goods, 6)
+    paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
->>>>>>> 1b16c7b (Normal pagination is added)
 
     context = {
-        "title": "Catalog Page",
+        "title": "Home - Каталог",
         "goods": current_page,
-        'slug_url': category_slug
-
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
-
     product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
     return render(request, "goods/product.html", context=context)
-
